787410458a974ec837c00a50aaf572e6c4f772d6
Dion Gerritzen <Diongerritzen@gmail.com>
2015-12-28 09:56:57 +0100

    updated models + created initial database

--- MoneyCounter/Models/TransactionModel.Designer.cs
+++ MoneyCounter/Models/TransactionModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'c:\users\student\documents\visual studio 2013\Projects\MoneyCounter\MoneyCounter\Models\TransactionModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Student\documents\visual studio 2013\Projects\MoneyCounter\MoneyCounter\Models\TransactionModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
